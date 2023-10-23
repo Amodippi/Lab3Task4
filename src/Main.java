7bc4abfcf62784787892ae0e3868501f7e6de1ed
Amodippi <w@tgxAeZcS6RYwS>
2023-10-23 13:51:07 +0200

    1.1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        int a = 1;
+        for (int i = 1; i < 100; i++){
+            System.out.println(a);
+            a = a + i;
 
+
+
+
+        }
     }
 }
